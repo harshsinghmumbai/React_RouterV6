b4e577108d5f76a4a3d4f28b0550e133dbd81005
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-12-28 13:06:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# React_RouterV6
+ Through this I have Learned React Router implementing in App
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="">
+        <h1 className="text-2xl font-semibold text-red-600">Hello World</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
