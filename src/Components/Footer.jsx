eedb3d14cb3ac2a486f26d9b348f263d661c0eb1
Harsh-Singh09 <harshrsingh552005@gmail.com>
2024-01-04 15:16:49 +0530

    sucessfully created footer part of webapp

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,44 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="w-full h-[10rem] flex justify-center items-center border-t-2">
+        <div className="flex w-full justify-around">
+          <p className="h-[5.78rem]">
+            <img
+              className="w-[17.75rem] h-[5.75rem]"
+              src="Images/logo_1.png"
+              alt=""
+            />
+          </p>
+          <div className="w-[42rem] flex justify-evenly h-[5rem] ">
+            <p className="inline-block w-[10rem]">
+              <h1 className="text-lg font-semibold ml-2 uppercase">Resource</h1>
+              <ul className="font-semibold ml-2 font-serif mt-1 leading-8">
+                <li>Home</li>
+                <li>About</li>
+              </ul>
+            </p>
+            <p className="inline-block w-[10rem]">
+              <h1 className="text-lg font-semibold ml-2 uppercase">
+                Follow us
+              </h1>
+              <ul className="font-semibold ml-2 font-serif mt-1 leading-8">
+                <li>Github</li>
+                <li>Discord</li>
+              </ul>
+            </p>
+            <p className="inline-block w-[12rem]">
+              <h1 className="text-lg font-semibold ml-2 uppercase">legal</h1>
+              <ul className="font-semibold ml-2 font-serif mt-1 leading-8">
+                <li>Privacy Policy</li>
+                <li>Terms and Coditions</li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/Components/MainHeader.jsx
+++ src/Components/MainHeader.jsx
@@ -1,16 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const MainHeader = () => {
   return (
     <>
       <Header />
       <Outlet />
-      <footer className="bg-red-700 w-full h-40">
-        <h1 className="text-7xl font-bold text-white font-sans">
-          this is my footer section in react routerV6 WebApplication project
-        </h1>
-      </footer>
+    <Footer/>
     </>
   );
 };
